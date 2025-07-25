4fd0419d50494ec1720f3362eb95bbb67bf2526b
장강민 <139250344+gangmin0716@users.noreply.github.com>
2025-07-26 02:48:22 +0900

    [Bronze I] Title: 소인수분해, Time: 1372 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+if N == 1:
+    print("")
+
+else:
+    for i in range(2, N+1):
+        if N % i == 0:
+            while N % i == 0:
+                print(i)
+                N = N / i
